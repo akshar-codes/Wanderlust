dcd94ad7b63a40d6b21ad0e0a67c249f1f5ccfe3
Akshar Gupta <akshargupta206@gmail.com>
2026-06-12 13:18:33 +0530

    Done V2.80

--- backend/tests/emailVerification.test.js
+++ backend/tests/emailVerification.test.js
@@ -2,7 +2,7 @@
 "use strict";
 
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const mongoose = require("mongoose");
 const crypto = require("crypto");
